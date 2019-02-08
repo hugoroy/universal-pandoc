8f1a5e7d715c0061c47093ebd4c2266c83c965d1
Hugo Roy <hugo@hugoroy.eu>
2019-02-08 22:49:29 +0100

    handle bigskips

--- html.sh
+++ html.sh
@@ -6,6 +6,7 @@ sed 's#"html","<label>"#"html","<a id="#g'  | \
     sed 's#"html","<\/vref>"#"html",">~VRef.<\/a>"#g' | \
     sed 's#"html","<ref>"#"html","<a href=\#"#g'  | \
     sed 's#"html","<\/ref>"#"html",">~Ref.<\/a>"#g' | \
+    sed 's#"html","<bigskip>"#"html","<br class=bigskip>"#g' | \
     sed 's#"html","<hg>"#"html","<span class=hangguillemet>« <\/span>"#g'| \
     sed 's#"html","<hq>"#"html","<span class=hangquote>“<\/span>"#g'
     
--- markdown.sh
+++ markdown.sh
@@ -6,5 +6,6 @@ sed 's#"html","<label>"#"html","<a id="#g'  | \
     sed 's#"html","<\/vref>"#"html","~VRef."#g' | \
     sed 's#"html","<ref>"#"html",""#g'  | \
     sed 's#"html","<\/ref>"#"html","~Ref."#g' | \
+    sed 's#"html","<bigskip>"#"html","   "#g' | \
     sed 's#"html","<hg>"#"html","« "#g'| \
     sed 's#"html","<hq>"#"html","“"#g'
